34503191831d283df5a6db57771ae92eebbd5960
cb.huang <qingchen191@gmail.com>
2024-09-05 14:33:14 +0800

    处理AliCloud AK

--- yy-admin-api/yy-base/src/main/java/com/yystudio/base/constant/SmsConst.java
+++ yy-admin-api/yy-base/src/main/java/com/yystudio/base/constant/SmsConst.java
@@ -2,8 +2,8 @@ package com.yystudio.base.constant;
 
 public class SmsConst {
 
-    public static final String ACCESS_KEY_ID = "LTAI5t7B9CYW2g1Ho6L862uR";
-    public static final String ACCESS_KEY_SECRET = "l2gcpmNTfu1vlVZZrxWw5EP2aFLdH9";
+    public static final String ACCESS_KEY_ID = System.getenv("YY_ALIBABA_CLOUD_ACCESS_KEY_ID");
+    public static final String ACCESS_KEY_SECRET = System.getenv("YY_ALIBABA_CLOUD_ACCESS_KEY_SECRET");
     // 临时签名，上线后申请正式的
     public static final String SIGN_NAME = "天津东丽区远游软件工作室";
     // 临时模板，上线后申请正式的
